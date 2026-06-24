4c4932c1cb7cd67e2a3439c269c43ab0be7717e9
arault <30930719+arault@users.noreply.github.com>
2026-06-24 11:28:44 +0200

    Commit initial

---
+++ 00_lab-lempnet/html/cloud.php
@@ -0,0 +1,62 @@
+<?php
+  // Variables Kubernetes
+  $node   = getenv('NODE_NAME');
+  $nodeip = getenv('NODE_IP');
+  $pod    = getenv('HOSTNAME');
+  $podip  = $_SERVER['SERVER_ADDR'];
+  
+  // Couleur stable par node
+  $hash   = crc32($nodeip);
+  $h      = $hash % 360;
+  $color  = "hsl($h,70%,45%)";
+  
+  // Gestion compteur local (par pod)
+  // Chaque pod garde son compteur dans un fichier temporaire
+  $counter_file = "/tmp/counter.txt";
+  if (!file_exists($counter_file))
+    file_put_contents($counter_file, "0");
+  
+  $count = (int)file_get_contents($counter_file);
+  $count++;
+  
+  file_put_contents($counter_file, $count);
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kubernetes Load Balancing Dashboard</title>
+    <meta http-equiv="refresh" content="2">
+    <link rel="stylesheet" type="text/css" href="cloud.css">
+    <style>
+      .bar{
+        height:20px;
+        border-radius:10px;
+        background: <?php echo $color ?>;
+        width:<?php echo min($count,100) ?>%;
+        transition: width 0.1s;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="header">Kubernetes Load Balancer Mini Dashboard</div>
+    <div class="container">
+      <div class="card">
+          <div class="node" style="background: <?php echo $color ?>">
+              Node : <?php echo $node ?>
+          </div>
+          <div class="info">
+            <table>
+             <tr><th>Node IP</th><td><?php echo $nodeip ?></td></tr>
+             <tr><th>Pod</th><td><?php echo $pod ?></td></tr>
+             <tr><th>Pod IP</th><td><?php echo $podip ?></td></tr>
+             <tr><th>Requêtes</th><td><?php echo $count ?></td></tr>
+            </table>
+          </div>
+          <div class="bar"></div>
+      </div>
+    </div>
+    <div class="footer">Auto refresh toutes les 2s</div>
+  </body>
+</html>
